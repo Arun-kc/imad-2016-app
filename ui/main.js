c10e560e2457d8589a3a4d440debcba7315b45bf
Arun-kc <arunkc91@gmail.com>
2016-09-22 19:18:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //move the image
-var image = document.getElementById('madi');
-madi.onclick=function(){
-    madi.style.marginLeft = '100px';
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight (){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick=function(){
+    var interval = setInterval(moveRight,100);
 };
