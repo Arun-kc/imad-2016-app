45f2e3596ea518cc801a7df26c7361f83b435fc1
Arun-kc <arunkc91@gmail.com>
2016-09-29 19:35:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,24 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
   
   //Create a request object
- // var request = new XMLHttpRequest();
+  var request = new XMLHttpRequest();
   
   //Capture a response and store it in a variable
- // request.onreadystatechange = function(){
-   // if (request.readyState === XMLHttpRequest.DONE){
+  request.onreadystatechange = function(){
+    if (request.readyState === XMLHttpRequest.DONE){
         //Take some action
-     //   if (request.status === 200){
-       //     var counter = request.responseText;
-            counter=counter+1;
+       if (request.status === 200){
+            var counter = request.responseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
-  //      }
-    //}  
+       }
+    }  
     //Not done yet
-//  };
+  };
   //Make a request
- // request.open('GET',"http://arun-kc.imad.hasura-app.io/counter",true);
+  request.open('GET','http://arun-kc.imad.hasura-app.io/counter',true);
   //request.sent(null);
 };
