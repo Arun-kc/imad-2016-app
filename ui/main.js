1d2d73fb85192733c1635149c423b394d6161757
Arun-kc <arunkc91@gmail.com>
2016-10-03 21:38:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,7 +44,7 @@ submit.onclick = function(){
       
                 }
             var ul = document.getElementById('namelist');
-             ul.innerHTNL=list;
+             ul.innerHTML=list;
        }
     }  
     //Not done yet
