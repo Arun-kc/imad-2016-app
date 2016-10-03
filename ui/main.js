c2f91121fd7b0052d56b83263e4f3cc0b921e303
Arun-kc <arunkc91@gmail.com>
2016-10-03 21:28:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,8 +28,32 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-  //Make a request to the server and sent the name
+ //Create a request object
+  var request = new XMLHttpRequest();
   
+  //Capture a response and store it in a variable
+  request.onreadystatechange = function(){
+    if (request.readyState === XMLHttpRequest.DONE){
+        //Take some action
+       if (request.status === 200){
+             //Capture a list of name and render it as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i=0;i<names.length;i++){
+                 list += '<li>'+names[i]+'</li>';
+      
+                }
+            var ul = document.getElementById('namelist');
+             ul.innerHTNL=list;
+       }
+    }  
+    //Not done yet
+  };
+  //Make a request
+  request.open('GET','http://arun-kc.imad.hasura-app.io/submit-name?name='+name,true);
+  request.send(null);
+
   //Capture a list of name and render it as a list
   var names = ['name1','name2','name3','name4'];
   var list = '';
